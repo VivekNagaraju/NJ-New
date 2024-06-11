5ad0e4321355bbd7093c559ae07ac129ed31fe69
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-06-11 08:24:31 +0530

    11-06-24

--- Practice/src/main/java/basics/DataTypes.java
+++ Practice/src/main/java/basics/DataTypes.java
@@ -1,9 +1,28 @@
 package basics;
 
 public class DataTypes {
+	
+	static int a;
+	static byte b;
+	static short c;
+	static long d;
+	static float e;
+	static double f;
+	static char g;
+	static boolean h;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		a=10;
+		
+		System.out.print(a);
+		System.out.print(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
 
 	}
 
