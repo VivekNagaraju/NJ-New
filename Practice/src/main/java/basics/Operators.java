8300e5f441f5dca2655975ad5c7c63b6a6f11b13
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-06-16 08:21:14 +0530

    16-06-24

---
+++ Practice/src/main/java/basics/Operators.java
@@ -0,0 +1,34 @@
+/*Operators: Are symbols which perform operations on Operands (Input values given to that operation).
+ * Ex:
+ * 
+ * Operation: Addition
+ * Operator: +
+ * 
+ *  Types/ classification of Operators:
+ *  1. Assignment operator
+ *  2. Comparison operators
+ *  3. Logical Operators
+ *  4. Relational Operators
+ *  5. Bitwise Operators
+ *  6. Ternary operators
+ *  7. Arithmetic Operators
+ * 
+ * */
+
+package basics;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 65;
+		int c = a + b; // addition
+		System.out.println("Sum of a and b is:" + c); // Concatenation
+		System.out.println("Sum of " + a + " and " + b + " is:" + c);
+//		System.out.printf("Sum of %d and %d is %d", a, b, c); // formatting and parameterization of output string
+//		System.out.println(a+b);
+		
+
+	}
+
+}
