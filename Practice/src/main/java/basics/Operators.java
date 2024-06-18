4ee2e8a2f5ed7fe64a9ca367dba8b25f5684181f
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-06-18 08:34:57 +0530

    18-06-24

--- Practice/src/main/java/basics/Operators.java
+++ Practice/src/main/java/basics/Operators.java
@@ -1,17 +1,22 @@
-/*Operators: Are symbols which perform operations on Operands (Input values given to that operation).
+/*
+ * Operators: Symbols which perform operations on Operand/s (Input values given to that operation).
+ * 
  * Ex:
  * 
  * Operation: Addition
  * Operator: +
  * 
  *  Types/ classification of Operators:
- *  1. Assignment operator
- *  2. Comparison operators
+ *  1. Assignment operator: =, +=, -=, *=, /=, %=  ; i/o: numeric
+ *  2. Comparison/ relational operators: ==, !=, >, <, >=, <=; i/p: numeric, o/p: boolean
  *  3. Logical Operators
- *  4. Relational Operators
- *  5. Bitwise Operators
- *  6. Ternary operators
- *  7. Arithmetic Operators
+ *  4. Bitwise Operators
+ *  5. Arithmetic Operators:+, -, *, /, %, ++, -- ; i/o: numeric
+ *  
+ *  Classification 2:
+ *  1. Unary Operators: ++, --
+ *  2. Binary Operators: +, -, ...
+ *  3. Ternary Operators: 
  * 
  * */
 
@@ -20,15 +25,43 @@ package basics;
 public class Operators {
 
 	public static void main(String[] args) {
-		int a = 12;
-		int b = 65;
-		int c = a + b; // addition
-		System.out.println("Sum of a and b is:" + c); // Concatenation
-		System.out.println("Sum of " + a + " and " + b + " is:" + c);
-//		System.out.printf("Sum of %d and %d is %d", a, b, c); // formatting and parameterization of output string
-//		System.out.println(a+b);
-		
+		int a = 2;
+		int b = 4;
+		/*
+		 * int c = a + b; // addition System.out.println("Sum of a and b is:" + c); //
+		 * Concatenation System.out.println("Sum of " + a + " and " + b + " is:" + c);
+		 * System.out.printf("Sum of %d and %d is %d", a, b, c); // formatting and
+		 * parameterization of output string System.out.println(a+b);
+		 * System.out.println(a^b);
+		 */
 
+		/* Increment(++)/ Decrement(--) Operators */
+		/*
+		 * System.out.println("Value of a before increment:" + a); int e=a++; // Post
+		 * Increment- first value will be assigned and then incremented
+		 * System.out.println("Value of e:"+e);
+		 * System.out.println("Value of a after increment:" + a);
+		 * System.out.println("=============================");
+		 * System.out.println("Value of b before increment:" + b); int f=++b; // Pre
+		 * Increment- first increments and value will be assigned later
+		 * System.out.println("Value of f:"+f);
+		 * System.out.println("Value of b after increment:" + b);
+		 */
+		
+		/* Assignment Operators */
+		int x=5;
+//		int y = x+=5; // x=x+5
+//		System.out.println(y);
+//		System.out.println(x);
+		
+		int z=x*=2;
+		System.out.println(z);
+		System.out.println(x);
+		
+		
+		
+		
+		
 	}
 
 }
