b6e60a12714d944587a089466e6dddb2f00e81e4
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-06-20 08:20:19 +0530

    20-06-24

--- Practice/src/main/java/basics/Operators.java
+++ Practice/src/main/java/basics/Operators.java
@@ -9,13 +9,39 @@
  *  Types/ classification of Operators:
  *  1. Assignment operator: =, +=, -=, *=, /=, %=  ; i/o: numeric
  *  2. Comparison/ relational operators: ==, !=, >, <, >=, <=; i/p: numeric, o/p: boolean
- *  3. Logical Operators
- *  4. Bitwise Operators
+ *  3. Logical Operators: AND --> &&; OR --> ||, NOT --> !; i/p: boolean (TRUE/ FALSE), o/p: boolean
+ *  
+ *  AND Logic: When both the i/ps are TRUE o/p will be TRUE
+ *  |  a  |  b  |  o/p  |
+ *  |TRUE |TRUE |TRUE   |
+ *  |TRUE |FALSE|FALSE  |
+ *  |FALSE|TRUE |FALSE  |
+ *  |FALSE|FALSE|FALSE  |
+ *  
+ *  OR Logic: When both the i/ps are TRUE o/p will be TRUE
+ *  |  a  |  b  |  o/p  |
+ *  |TRUE |TRUE |TRUE   |
+ *  |TRUE |FALSE|TRUE   |
+ *  |FALSE|TRUE |TRUE   |
+ *  |FALSE|FALSE|FALSE  |
+ *  
+ *  NOT Logic: Reverse the i/p; it requires only one i/p
+ *  |  a  | o/p |
+ *  |TRUE |FALSE|
+ *  |FALSE|TRUE |
+ *  
+ *  
+ *  4. Bitwise Operators: 
+ *  	Bitwise AND, & :
+ *  	Bitwise OR , | :
+ *  	Bitwise XOR, ^ :
+ *  	Bitwise NOT, ~ :
  *  5. Arithmetic Operators:+, -, *, /, %, ++, -- ; i/o: numeric
+ *  6. Shift Operator
  *  
  *  Classification 2:
- *  1. Unary Operators: ++, --
- *  2. Binary Operators: +, -, ...
+ *  1. Unary Operators: ++, --, =
+ *  2. Binary Operators: +, -,  AND --> &&; OR --> ||
  *  3. Ternary Operators: 
  * 
  * */
@@ -25,8 +51,9 @@ package basics;
 public class Operators {
 
 	public static void main(String[] args) {
-		int a = 2;
-		int b = 4;
+		/*
+		 * int a = 2; int b = 4;
+		 */
 		/*
 		 * int c = a + b; // addition System.out.println("Sum of a and b is:" + c); //
 		 * Concatenation System.out.println("Sum of " + a + " and " + b + " is:" + c);
@@ -49,18 +76,44 @@ public class Operators {
 		 */
 		
 		/* Assignment Operators */
-		int x=5;
+//		int x=5;
 //		int y = x+=5; // x=x+5
 //		System.out.println(y);
 //		System.out.println(x);
 		
-		int z=x*=2;
-		System.out.println(z);
-		System.out.println(x);
+		/*
+		 * int z=x*=2; System.out.println(z); System.out.println(x);
+		 */
 		
 		
+		/* Comparison/ relational operators */
+		/*
+		 * System.out.println(2==3); System.out.println(4==4);
+		 */
 		
+		/* Logical Operators */
+		/* AND Operator */
+		System.out.println("========AND Operator========");
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
 		
+		/* OR operator */
+		System.out.println("========OR Operator========");
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+		
+		/* NOT Operator */
+		System.out.println("========NOT Operator========");
+		System.out.println(!true);
+		System.out.println(!false);
+		
+		/* Bitwise Operators */
+		System.out.println("========Bitwise Operators========");
+		System.out.println(5 & 6);
 		
 	}
 
