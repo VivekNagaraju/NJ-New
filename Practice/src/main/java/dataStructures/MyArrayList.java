c4f24193559096f6d56ad4cf88ca0ee771019a33
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-07-17 08:53:06 +0530

    17-07-24

--- Practice/src/main/java/dataStructures/MyArrayList.java
+++ Practice/src/main/java/dataStructures/MyArrayList.java
@@ -1,3 +1,11 @@
+/**
+ ArrayList: Resizable-array implementation of the List interface.
+ 
+ -> An empty arrayList can be created
+ -> ArrayList holds null values
+ -> Objects/ elements are stored in a sequence; insertion order is preserved 
+ 
+ */
 package dataStructures;
 
 import java.util.ArrayList;
@@ -6,9 +14,26 @@ import java.util.ArrayList;
 public class MyArrayList {
 
 	public static void main(String[] args) {
-		// ArrayList<Type> objectName = new ArrayList<Type>();
+		// ArrayList<DataType Wrapper Class> objectName = new ArrayList<DataType Wrapper Class>();
 		
 		ArrayList<Integer> arrayList1 = new ArrayList<Integer>();
+		System.out.println(arrayList1);
+		arrayList1.add(null);
+		System.out.println(arrayList1);
+		arrayList1.add(5);
+//		arrayList1.add(3);
+//		arrayList1.add(4);
+//		arrayList1.add(6);
+//		arrayList1.add(8);
+		System.out.println(arrayList1);
+		System.out.println(arrayList1.get(0));
+		System.out.println(arrayList1.get(1));
+//		System.out.println("Negative index"+arrayList1.get(-1)); // Negative index is not supported
+//		System.out.println(arrayList1.indexOf(6));
+//		System.out.println(arrayList1.indexOf(8));
+		System.out.println(arrayList1.indexOf(1)); // return -1 because element is not present in the list
+		
+		
 		
 
 	}
