d9cabfc47c0c2ca11246ce868dba4601656d1e5c
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-08-04 16:18:46 +0530

    04-08-2024

---
+++ SeleniumTutorial/src/main/java/basics/ValidationInSelenium.java
@@ -0,0 +1,35 @@
+package basics;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class ValidationInSelenium {
+
+	public static void main(String[] args) {
+		// 1. Launching the Chrome browser
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().maximize();
+				System.out.println("Chrome browser launched");
+//				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+				
+				// 2. Navigating to test site
+				driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+				System.out.println("Navigated to Orange HRM site");
+				
+				//3. Validation
+				WebElement loginTitle=driver.findElement(By.tagName("h5"));
+				
+				  if(loginTitle.isDisplayed() == true) {
+				  
+				  System.out.println("TEST PASSED"); }else { System.out.println("TEST FAILED");
+				  }
+				 
+				
+				
+	}
+
+}
