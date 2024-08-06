4d6bcf04939135add9c7d54b9c7d4f2c141d62a3
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-08-06 08:37:52 +0530

    06-08-24

--- SeleniumTutorial/src/test/java/junitTestCases/MyJunitTest.java
+++ SeleniumTutorial/src/test/java/junitTestCases/MyJunitTest.java
@@ -9,13 +9,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class MyJunitTest {
 
 	@Test
 	public void testOrangeHRMNavigation() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("incognito");
 		// 1. Launching the Chrome browser
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		System.out.println("Chrome browser launched");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -28,6 +31,32 @@ public class MyJunitTest {
 		WebElement loginTitle=driver.findElement(By.tagName("h5"));
 //		assertTrue(loginTitle.isDisplayed());
 		assertFalse(loginTitle.isDisplayed());
+	}
+	
+	@Test
+	public void testOrangeHRMLogin() {
+		// 1. Launching the Chrome browser
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().maximize();
+				System.out.println("Chrome browser launched");
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+				
+				// 2. Navigating to test site
+				driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+				System.out.println("Navigated to Orange HRM site");
+				
+				//3. Enter username, password and click on  login button
+				WebElement usernametxtbx=driver.findElement(By.name("username"));
+				usernametxtbx.sendKeys("Admin");
+				
+				WebElement passwordtxtbx=driver.findElement(By.name("password"));
+				passwordtxtbx.sendKeys("admin123");
+				
+				WebElement loginBtn=driver.findElement(By.tagName("button"));
+				loginBtn.click();
+				
+				
+		
 	}
 
 }
